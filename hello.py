f47238b96a4fdf6ecc87febe9b020bc6ed58c624
senthilvivekanandan-hautelook <senthil.vivekanandan@hautelook.com>
2017-12-20 12:37:22 -0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+print 'Hello, world!'
+
+name = raw_input('What is your name?')
+print 'Hi %s.' % name
+
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print "iteration {iteration} is {name}".format(iteration=i, name=name);
+
+    #for i, name in ()
+
+    for j, name in enumerate (friends):
+
+
